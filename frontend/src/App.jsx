4c95a5606374b5d967308cbccc670f0d3e513028
Sonualam <sonualam1599@gmail.com>
2024-01-27 18:49:53 +0530

    completed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+// import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+// import Loader from "./components/Loader";
+// const Signup = lazy(() => import("./pages/Signup"));
+// const Signin = lazy(() => import("./pages/Signin"));
+// const Dashboard = lazy(() => import("./pages/Dashboard"));
+// const SendMoney = lazy(() => import("./pages/SendMoney"));
+import { Dashboard } from "./pages/Dashboard";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import SendMoney from "./components/SendMoney";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      {/* <Suspense fallback={<Loader />}> */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+      {/* </Suspense> */}
+    </>
+  );
 }
 
-export default App
+export default App;
